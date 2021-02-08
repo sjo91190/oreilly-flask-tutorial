5ddce25268c0390307d54bdda42e98b5760bf964
Sam O'Keefe <sjo91190@gmail.com>
2021-02-07 18:50:30 -0700

    syncing with main repo

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
 import os
-from flask_sqlalchemy import SQLAlchemy
-from typing import Callable
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.urandom(24)
-    MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.googlemail.com")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", "587"))
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "true").lower() in ["true", "on", "1"]
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    FLASKY_MAIL_SUBJECT_PREFIX = "[Flasky]"
-    FLASKY_MAIL_SENDER = "Flasky Admin <sjo91190.Develop@gmail.com>"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+        ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -24,29 +22,25 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "data-test.sqlite")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite://'
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "data.sqlite")
-
-
-class MySQLAlchemy(SQLAlchemy):
-    Column: Callable
-    String: Callable
-    Integer: Callable
-    relationship: Callable
-    ForeignKey: Callable
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+
     'default': DevelopmentConfig
 }
--- flasky.py
+++ flasky.py
@@ -1,20 +1,25 @@
 import os
+import click
+from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role
-from flask_migrate import Migrate
 
-
-app = create_app(os.environ.get('FLASK_CONFIG') or "default")
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Role": Role}
+    return dict(db=db, User=User, Role=Role)
 
 
 @app.cli.command()
-def test():
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
